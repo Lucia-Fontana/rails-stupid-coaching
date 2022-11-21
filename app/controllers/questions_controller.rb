093dad086df135cabf5b7857b0adf8bd6d725cfb
Lucia Fontana <lucia.fontana1987@gmail.com>
2022-11-21 14:06:18 +0100

    Form

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,15 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    # @questions = ["I am going to work"]
+    if params['question'] == 'I am going to work'
+      then @answer = 'Great!'
+    elsif params['question'].end_with?('?')
+      then @answer = 'Silly question, get dressed and go to work!'
+    else
+      @answer = 'I dont care, get dressed and go to work!'
+    end
+  end
+end
